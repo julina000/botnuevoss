0bc5b3855f1f8e951444965012258ea535802086
julina000 <101071434+julina000@users.noreply.github.com>
2022-04-18 23:43:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -636,7 +636,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5304501240:AAGGZA8QIMIjWlJCihu91XTawSy8a68E-AI '
+    bot_token = '5304501240:AAGGZA8QIMIjWlJCihu91XTawSy8a68E-AI'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
